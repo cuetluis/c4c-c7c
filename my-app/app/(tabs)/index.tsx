8481b6998938664e17ddb6dff2a774a898fe318f
Luis Cuetara <147174042+cuetluis@users.noreply.github.com>
2026-02-08 12:09:11 -0500

    Update index.tsx
    Background added.

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import {Text, View, Button, Pressable, Image, StyleSheet} from "react-native";
-import {useRouter, Link} from 'expo-router';
-import FullScreenImageScreen from '../../components/fullscreen';
+import {View, Image, StyleSheet, ImageBackground} from "react-native";
+import {Link} from 'expo-router';
+
+const logo = require("../assets/images/logo.png")
+const back_image = require("../assets/images/image.png");
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <FullScreenImageScreen />
-      <Image
-      />
+    <ImageBackground source = {back_image} style = {{flex: 1}}>
+    <View style = {styles.container}>
+      <Image source = {logo}/>
 
-      <Link href="/(auth)/login" style={{ padding: 10, backgroundColor: 'blue', color: 'white', borderRadius: 5 }}>
-        Go to Login
+      <Link href="/(auth)/login" style={{ padding: 20, fontSize: 30, fontWeight: 'bold', color: 'black', borderRadius: 5}}>
+        Login
       </Link>
-
     </View>
+    </ImageBackground>
   );
 }
 
 // Stylesheet
 const styles = StyleSheet.create({
   container: {
-    flex: 1, // Takes up the entire screen space
-    justifyContent: 'center', // Centers children vertically
-    alignItems: 'center', // Centers children horizontally
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
